713a31f7df889bb139098c1adf0c6775505c8a88
callikys <callikys@naver.com>
2024-05-16 17:58:21 +0900

    feat: 공지사항 관련 화면 제작 (공지사항 데이터 및 공지사항 화면)

---
+++ lib/screens/NoticeListScreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:devtogether/data/noticeList.dart';
+import 'package:devtogether/widgets/BaseAppBar.dart';
+import 'package:devtogether/widgets/groupWidgets/NoticeItem.dart';
+
+class NoticeListScreen extends StatefulWidget {
+  const NoticeListScreen({super.key});
+
+  @override
+  State<NoticeListScreen> createState() => _NoticeListScreenState();
+}
+
+class _NoticeListScreenState extends State<NoticeListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Scaffold(
+            appBar: BaseAppBar(appBar: AppBar(), title: "공지사항", center: true),
+            body: Container(
+                color: Colors.white,
+                padding: const EdgeInsets.fromLTRB(40, 30, 40, 30),
+                child: ListView.separated(
+                    separatorBuilder: (BuildContext ctx, int idx) {
+                      return const Divider(
+                        // height: 40,
+                      );
+                    },
+                    itemCount: notices_test_data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      dynamic notice = notices_test_data[index];
+                      return NoticeItemWidget(
+                          title: notice['title'],
+                          category: notice['category'],
+                          created_at: notice['created_at'],
+                          onPressed: () =>
+                          {context.push('/notice/detail', extra: notice)});
+                    }))));
+  }
+}
